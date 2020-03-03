bf9789490d69e5dac11427a209ad286e2a457ad0
camillgh <millagreveh@gmail.com>
2020-03-03 20:49:15 +0100

    feat(monitor): Add PollOrdersNetwork

--- src/internal/monitor/monitor.go
+++ src/internal/monitor/monitor.go
@@ -3,7 +3,7 @@ package monitor
 import (
 	//"fmt"
 	"time"
-
+	"encoding/json"
 	"pkg/elevio"
 )
 
@@ -120,4 +120,42 @@ func PollOrders(sender <-chan elevio.ButtonEvent) {
 		}
 	}
 	//TODO: Implement functionality to poll the network for new orders
+}
+
+
+//Make a struct with what needs to be sent; ElevStates and OrderMatrix (I called it ElevStates_OrderMatrix here)
+//Make a function that Marshals and transmits the messages through the network
+
+IncomingMsg := make(chan ElevStates_OrderMatrix)
+go bcast.Receiver(0, IncomingMsg)
+
+func PollOrdersNetwork(packet <-chan IncomingMsg) {
+	var msg ElevStates_OrderMatrix
+	err := json.Unmarshal(packet, &msg)
+	if err != nil {
+		fmt.Println("error with unmarshaling message:", err)
+	}
+
+	for floors := 0; floors < MFloors; i++ {
+		for elevs := 0; elevs < NElevs; j++ {
+			if elevs == Elev_id && msg.OrderMatrix[floors][elevs].Clear{
+			
+			OrderMatrix[floors][elevs].Up = false
+			OrderMatrix[floors][elevs].Down = false
+			OrderMatrix[floors][elevs].Cab = false
+							
+			}
+			if elevs == Elev_id && msg.OrderMatrix[floors][elevs].Cab{
+				OrderMatrix[floors][elevs].Cab = true				
+			}	
+				
+			if elevs == Elev_id && msg.OrderMatrix[floors][elevs].Up{ 
+				OrderMatrix[floors][elevs].Up = true
+			}
+	
+			if elevs == Elev_id && msg.OrderMatrix[floors][elevs].Down) {
+				OrderMatrix[floors][elevs].Down = true
+			}
+		}
+	}
 }
